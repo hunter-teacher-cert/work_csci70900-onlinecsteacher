7f6c6a3d99c4ee475389388e63bf91c44367d29e
Emma Wingreen <emmawingreen@gmail.com>
2021-07-12 12:36:01 -0400

    update Maze.java and Driver.java

--- ds/maze/Driver.java
+++ ds/maze/Driver.java
@@ -3,17 +3,19 @@ import java.util.*;
 
 public class Driver{
 
-    public static void main(String[] args) {
-	boolean solved;
-	/*
-	  Maze m = new Maze("maze.dat");
-	solved = m.solve(1,1);
-	*/
-	/*
-	  Knights k - new Knights(5);
-	  solved = k.solve(1,1);
-	*/
-	Knights k = new Knights(5);
-	k.solve(2,2,1);
-    }
+  public static void main(String[] args) {
+
+    boolean solved;
+
+  	Maze m = new Maze("maze.dat");
+    System.out.println(m);
+  	solved = m.solve(1,1);
+
+  	/*
+  	  Knights k - new Knights(5);
+  	  solved = k.solve(1,1);
+  	*/
+  	// Knights k = new Knights(5);
+  	// k.solve(2,2,1);
+  }
 }
