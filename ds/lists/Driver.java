02eeff5901e7c9bebcf644f175a3c49a88c17350
Emma Wingreen <emmawingreen@gmail.com>
2021-07-14 14:01:32 -0400

    Update Node and Driver

--- ds/lists/Driver.java
+++ ds/lists/Driver.java
@@ -2,62 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Driver{
-
-  public static void main(String[] args) {
-
-    Node n;
-  	n = new Node();
-  	n.setData("Eduardo");
+    public static void main(String[] args) {
+	Node n;
+	n = new Node();
+	n.setData("Eduardo");
 
 
-  	Node n2 = new Node("Brian");
-  	n.setNext(n2);
+	Node n2 = new Node("Brian");
+	n.setNext(n2);
 
-  	System.out.println(n);
+	System.out.println(n);
 
-  	System.out.println(n2);
+	System.out.println(n2);
 
-  	System.out.println(n.getNext());
+	System.out.println(n.getNext());
 
-    //Node n3 = new Node("Steph");
+  //Node n3 = new Node("Steph");
 
-  	n.getNext().setNext(new Node("Steph"));
-    n.getNext().getNext().setNext(new Node("Tofr"));
-  	System.out.println(n2.getNext());
-  	System.out.println(n.getNext().getNext());
+	n.getNext().setNext(new Node("Steph"));
+  n.getNext().getNext().setNext(new Node("Tofr"));
+	//System.out.println(n2.getNext());
+	//System.out.println(n.getNext().getNext());
 
-  	Node L = new Node("Tiffany");
-    L.setNext(n);
+  //create new list
+	Node L = new Node("Tiffany");
+  L.setNext(n);
 
-    System.out.println(L);
+  System.out.println("Create a new  list that looks like: L->a->b->c->d ");
+  L.printNodeList();
 
-    System.out.println(n.getNext().getNext().getNext());
+  //System.out.println(L);
 
-    Node x = new Node("Mamudu");
-    x.setNext(n2); //Mamudu to Brian
-    n.setNext(x); //Eduardo to Mamudu
+  //System.out.println(n.getNext().getNext().getNext());
 
-    //prints out linked list
-    System.out.println("This is a linked list: ");
-    Node s = L;
-    while(s != null){
-      System.out.println(s);
-      s = s.getNext();
-    }
-    x.setNext(n2.getNext());
-    //prints out linked list
-    System.out.println("This is a linked list: ");
-    Node s2 = L;
-    while(s2 != null){
-      System.out.println(s2);
-      s2 = s2.getNext();
-    }
-    //Node
-  	// 1.Create a new  list that looks like:
-  	//   L->"a"->"b"->"c"->"d"
-  	// 2. Write the code to insert an "x"
-  	//    between the b and the c
-  	// 3. Write the code to delete the c
+  //insert new node in linked list
+  Node x = new Node("Mamudu");
+  x.setNext(n2); //Mamudu to Brian
+  n.setNext(x); //Eduardo to Mamudu
+
+  System.out.println("Write the code to insert an x between b and c");
+  L.printNodeList();
+
+  //code to delete a node
+  x.setNext(n2.getNext());
+  System.out.println("Write the code to delete c");
+  L.printNodeList();
+  //Node
+	// 1.Create a new  list that looks like:
+	//   L->"a"->"b"->"c"->"d"
+	// 2. Write the code to insert an "x"
+	//    between the b and the c
+	// 3. Write the code to delete the c
+
 
     }
 }
--- ds/lists/Node.java
+++ ds/lists/Node.java
@@ -3,27 +3,27 @@ import java.util.*;
 
 public class Node {
 
-  private String name;
+  private String data;
   private Node next;
 
   public Node() {
-    name = "";
+    data = "";
     next = null;
   }//default constructor
 
   public Node(String value) {
 
-    name = value;
+    data = value;
     next = null;
-  }//constructor(value)
+  }//constructor(vale)
 
   public Node(String value, Node next) {
-    name = value;
+    data = value;
     this.next = next;
   }//constructor(value, next)
 
   public void setData(String value) {
-    name = value;
+    data = value;
   }//setData
 
   public void setNext(Node n) {
@@ -31,15 +31,23 @@ public class Node {
   }//setNext
 
   public String getData() {
-    return name;
-  }//end getData
+    return data;
+  }//end getValue
 
   public Node getNext() {
     return next;
   }//getNext
 
   public String toString() {
-    return name;
+    return data;
   }//toString
+
+  public void printNodeList(){
+    Node s = this;
+    while(s != null){
+      System.out.println(s);
+      s = s.getNext();
+    }
+  }
 
 }//class Node
