13e4831be0fe9e41718142a085d2a8800615f6a0
Emma Wingreen <emmawingreen@gmail.com>
2021-07-29 10:46:57 -0400

    Add knights folder

---
+++ ds/knights/Driver.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+
+  public static void main(String[] args) {
+
+    boolean solved;
+
+/*
+  	Maze m = new Maze("maze.dat");
+    //System.out.println(m);
+  	solved = m.solve(1,1);
+  */
+
+	  Knights k = new Knights(5);
+    System.out.println(k);
+	  solved = k.solve(2,2,0);
+
+  	// Knights k = new Knights(5);
+  	// k.solve(2,2,1);
+
+
+  }
+}
